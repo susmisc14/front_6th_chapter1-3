846d4f832d5b2db3ccf735e512f209762676b4b5
susmisc14 <su.smisc14@gmail.com>
2025-07-22 00:18:31 +0900

    feat: 기본 과제 HOC 구현

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,17 @@
-import { type FunctionComponent } from "react";
+import { type FunctionComponent, createElement } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  return (props: P) => {
+    const prevProps = useRef<P>();
+    const prevElement = useRef<React.ReactElement>();
+
+    if (!equals(props, prevProps.current)) {
+      prevProps.current = props;
+      prevElement.current = createElement(Component, props);
+    }
+
+    return prevElement.current;
+  };
 }
