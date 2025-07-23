33874361b55a34aaf8c2f66dc76f11a5f0b9af14
susmisc14 <su.smisc14@gmail.com>
2025-07-23 16:31:40 +0900

    feat: 심화 과제 커스텀 훅 구현

--- packages/lib/src/hooks/useShallowSelector.ts
+++ packages/lib/src/hooks/useShallowSelector.ts
@@ -1,9 +1,20 @@
-import { useRef } from "react";
 import { shallowEquals } from "../equals";
+import { useAutoCallback } from "./useAutoCallback";
+import { useRef } from "./useRef";
 
 type Selector<T, S = T> = (state: T) => S;
 
 export const useShallowSelector = <T, S = T>(selector: Selector<T, S>) => {
   // 이전 상태를 저장하고, shallowEquals를 사용하여 상태가 변경되었는지 확인하는 훅을 구현합니다.
-  return (state: T): S => selector(state);
+  const prevValue = useRef<S>();
+
+  return useAutoCallback((state: T): S => {
+    const nextValue = selector(state);
+
+    if (prevValue.current === undefined || !shallowEquals(nextValue, prevValue.current)) {
+      prevValue.current = nextValue;
+    }
+
+    return prevValue.current;
+  });
 };
